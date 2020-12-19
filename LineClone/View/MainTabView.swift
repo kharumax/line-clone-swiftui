069750fae398ca27a6c6b21b0c924b59ccc7d082
kharumax <kharumax@gmail.com>
2020-12-20 02:45:36 +0900

    Create Folders

---
+++ LineClone/View/MainTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MainTabView.swift
+//  LineClone
+//
+//  Created by 久保田陽人 on 2020/12/20.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
