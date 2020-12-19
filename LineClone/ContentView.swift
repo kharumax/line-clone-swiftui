57aae9a2f021b94b86eddb949a26e97050ab4b44
kharumax <kharumax@gmail.com>
2020-12-20 02:35:09 +0900

    First Commit

---
+++ LineClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LineClone
+//
+//  Created by 久保田陽人 on 2020/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LineClone/LineCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineCloneApp.swift
+//  LineClone
+//
+//  Created by 久保田陽人 on 2020/12/20.
+//
+
+import SwiftUI
+
+@main
+struct LineCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
